f8e34c83b477586b23426b9bbbdec897b619129e
Mark Joseph Pardiñas <markjoseph.pardinas@unc.edu.ph>
2024-07-02 22:49:40 +0800

    programhead ui

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\PHHomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
+// Route for displaying the login form
+Route::get('/',[AuthenticationController::class, 'create']);
 
-//Route::get('/login', [AuthController::class,'login'])->name('login');
+// Route for handling the login process
+Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
+
+// Route for the login page
+Route::get('/login',[AuthenticationController::class, 'create'])->name('create');
+
+// ROUTES FOR PROGRAM HEAD UI
+Route::get('/programhead/home',[PHHomeController::class, 'index'])->name('home');
+Route::get('/programhead/plan',[PHHomeController::class, 'plan'])->name('plan');
+Route::get('/programhead/questionbank',[PHHomeController::class, 'questionbank'])->name('questionbank');
+Route::get('/programhead/class',[PHHomeController::class, 'class'])->name('class');
+Route::get('/programhead/settings',[PHHomeController::class, 'settings'])->name('settings');
+
