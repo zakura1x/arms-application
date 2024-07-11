ed640c911406c5bab4f98adbd516652f1145a73a
Mark Joseph Pardiñas <markjoseph.pardinas@unc.edu.ph>
2024-07-11 21:30:08 +0800

    back icon

--- resources/views/programhead/css.blade.php
+++ resources/views/programhead/css.blade.php
@@ -2,12 +2,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2" defer></script>
 
     <title>Accountancy Review Management</title>
 
     @vite('resources/css/app.css')
     <style>
+        body {
+            background-color: red;
+        }
+
         #sidebar {
             transition: transform 0.3s ease-in-out, width 0.3s ease-in-out;
             width: 4rem;
